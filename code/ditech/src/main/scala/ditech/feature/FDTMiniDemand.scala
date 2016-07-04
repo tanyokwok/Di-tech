1dec6a4da62a977dcbe2a1633748b210e5c298b3
fortianyou <fortianyou@gmail.com>
2016-07-04 15:13:20 +0800

    add FDTMini*

--- code/ditech/src/main/scala/ditech/feature/FDTMiniDemand.scala
+++ code/ditech/src/main/scala/ditech/feature/FDTMiniDemand.scala
@@ -4,14 +4,14 @@ import com.houjp.ditech16
 import ditech.datastructure.OrderAbs
 import ditech.feature.ftrait.FDTMiniTrait
 
-object FDTMiniGap extends FDTMiniTrait{
+object FDTMiniDemand extends FDTMiniTrait{
 
   def main(args:Array[String]): Unit ={
     run(ditech16.data_pt,this.getClass.getSimpleName.replace("$",""))
   }
 
   override def collect_order( ord:OrderAbs, fs:collection.mutable.Map[(Int,Int,Int), Double]): Unit ={
-    if (-1 != ord.start_district_id && !ord.has_driver) {
+    if (-1 != ord.start_district_id) {
       val tid = ord.time_id
       val b_mid = (tid - 1) * 10 + 1
       val mid = ord.min_time_id - b_mid
